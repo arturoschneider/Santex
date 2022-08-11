ebd9e5aa54b2859a865ac9a609fdca1acb1736fc
Artur Schneider <arturoschneider@mi.unc.edu.ar>
2022-08-11 15:00:39 -0300

    Commit for the first Angular homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'secondProject';
+
+  contador = 0;
+
+  sumarUno(): void {
+    this.contador += 1;
+
+  }
+  names = [ "Agus", "Tomas", "Pepo", "Zara"];
+
+  random = Math.floor(Math.random() * this.names.length);
+  name = this.names[this.random]
+
+  mostrarNombre(name: string, price: number): void {
+
+    console.log(name)
+    console.log(price)
+  }
+
+  products = [{name: "Mouse", "price": 10}, {name: "SSD", "price": 200}, {name:"Monitor", "price": 40}, {name:"RAM", price: 25}, {name: "SSD", price: 25}]// "HDD": 15, "Teclado": 30, "Silla": 60, "Auris": 50, "Cámara": 50, "Cooler": 60, "Fuente": 80} ]
+  isButtonVisible = true;
+  showHide(){
+    this.isButtonVisible = !this.isButtonVisible;
+  }
 }
+
